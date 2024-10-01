d0fe127be65c6f06422618f7b66aa1b481c2a358
Hwangmyeongje <hmj1438@naver.com>
2024-10-01 23:23:28 +0900

    first commit

---
+++ routes/orders.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+
+router.use(express.json());
+
+//주문하기
+router.post("/", (req, res) => {
+  res.json("주문 하기");
+});
+
+//주문 목록 조회
+router.post("/", (req, res) => {
+  res.json("주문 목록 ㅈ회");
+});
+
+//주문 상세 상품 조회
+router.get("/:id", (req, res) => {
+  res.json("주문 상세 상품 조회");
+});
+
+module.exports = router;
